d31cd3e3f4cb8c301aeb21ce8a1d9e4f74308449
Georgiy-Vwfe <31819579+Georgiy-Vwfe@users.noreply.github.com>
2019-04-13 12:49:37 +0300

    refactor code

--- src/ru/ithub/Main.java
+++ src/ru/ithub/Main.java
@@ -3,53 +3,23 @@ package ru.ithub;
 public class Main {
 
     public static void main(String[] args) {
-        String str = "14";
+        String str = "999999";
 
         System.out.println("Before: " + str);
         System.out.println("After: " + stringIncrement(str));
     }
 
     private static String stringIncrement(String str) {
-        String lastChar = str.substring(str.length() - 1);
+        char lastChar = str.charAt(str.length() - 1);
         String withoutLastChar = str.substring(0, str.length() - 1);
 
-        switch (lastChar) {
-            case "0": {
-                return withoutLastChar + "1";
-            }
-            case "1": {
-                return withoutLastChar + "2";
-            }
-            case "2": {
-                return withoutLastChar + "3";
-            }
-            case "3": {
-                return withoutLastChar + "4";
-            }
-            case "4": {
-                return withoutLastChar + "5";
-            }
-            case "5": {
-                return withoutLastChar + "6";
-            }
-            case "6": {
-                return withoutLastChar + "7";
-            }
-            case "7": {
-                return withoutLastChar + "8";
-            }
-            case "8": {
-                return withoutLastChar + "9";
-            }
-            case "9": {
-                if (!withoutLastChar.equals("")) {
-                    return stringIncrement(withoutLastChar) + "0";
-                } else {
-                    return "10" + lastChar.substring(1);
-                }
-            }
-            default: {
-                return "Unknown Error";
+        if (lastChar > '0' && lastChar < '9') {
+            return withoutLastChar + (++lastChar);
+        } else {
+            if (!withoutLastChar.equals("")) {
+                return stringIncrement(withoutLastChar) + "0";
+            } else {
+                return "10";
             }
         }
     }
